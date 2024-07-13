7567f9136fdd0e2e02eeabd356bc35b6c525757a
Riggs <xconnorx@gmx.de>
2024-07-14 00:30:56 +0200

    Update vwep.lua

--- lua/autorun/vwep.lua
+++ lua/autorun/vwep.lua
@@ -14,18 +14,19 @@ include("vwep/sh_util.lua")
 
 vwep.util:IncludeDir("vwep/modules")
 
+local install = "https://github.com/Minerva-Servers/vwep/archive/refs/heads/main.zip"
 http.Fetch("https://raw.githubusercontent.com/Minerva-Servers/vwep/main/VERSION.txt", function(body)
     if ( body == vwep.info.version ) then
         if ( SERVER ) then
-            vwep.util:Message("You are using the latest version of " .. vwep.info.name .. " (" .. vwep.info.version .. "). No updates are available.")
+            vwep.util:Message("You are using the latest version of " .. vwep.info.name .. " (" .. vwep.info.version .. ").")
         else
-            vwep.util:Message("This server is using the latest version of " .. vwep.info.name .. " (" .. vwep.info.version .. "). No updates are available.")
+            vwep.util:Message("This server is using the latest version of " .. vwep.info.name .. " (" .. vwep.info.version .. ").")
         end
     else
         if ( SERVER ) then
-            vwep.util:Message(Color(255, 0, 0), "You are using an outdated version of " .. vwep.info.name .. " (" .. vwep.info.version .. "). An update is available at " .. install .. ".")
+            vwep.util:Message(Color(255, 0, 0), "You are using an outdated version of " .. vwep.info.name .. " (" .. vwep.info.version .. "). The latest version is " .. body .. " and can be downloaded using the following link: " .. install .. ".")
         else
-            vwep.util:Message(Color(255, 0, 0), "This server is using an outdated version of " .. vwep.info.name .. " (" .. vwep.info.version .. "). An update is available at " .. install .. ".")
+            vwep.util:Message(Color(255, 0, 0), "This server is using an outdated version of " .. vwep.info.name .. " (" .. vwep.info.version .. "). The latest version is " .. body .. " and can be downloaded using the following link: " .. install .. ".")
         end
     end
 end)
